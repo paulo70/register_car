e2e98ec7bf69c025d707e4e012cd922032a9bc11
Paulrocker <paulofrontend@gmail.com>
2017-08-12 13:21:28 -0300

    request ajax

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -4,11 +4,19 @@
     const $companyTitle = $('[data-js="companytitle"]');
     const $companyPhone = $('[data-js="companyphone"]');
     const $sendPOST = $('[data-js="post"]');
+    const $sendGET = $('[data-js="get"]');
     const $contentTable = $('[data-js="content"]');
     const $inputs = $('[data-js="input"]');
+    const $getCar = $('[data-js="getcar"]');
 
     const ajxCompany = new XMLHttpRequest();
+    const ajxPost = new XMLHttpRequest();
+    const ajxGET = new XMLHttpRequest();
 
+    function initEvents() {
+      $sendPOST.addEventListener('submit', handleForm, false);
+      $sendGET.addEventListener('submit', handleFormGET, false);
+    }
 
     function requestGetCompany() {
       ajxCompany.open('GET', 'company.json');
@@ -17,24 +25,12 @@
       ajxCompany.addEventListener('readystatechange', handleInfo, false);
     }
 
-    function initEvents() {
-      $sendPOST.addEventListener('submit', handlePost, false);
-    }
-
     function handleInfo() {
       if (isRequestOK(ajxCompany)) {
         var data = parserData(ajxCompany);
         $companyTitle.innerHTML = data.company;
         $companyPhone.innerHTML = data.phone;
       }
-    }
-
-    function isRequestOK(name) {
-      return name.readyState === 4 && name.status === 200;
-    }
-
-    function parserData(name) {
-      return JSON.parse(name.responseText);
     }
 
     function createButtonRemove() {
@@ -62,18 +58,17 @@
     function createTableElements() {
       const $fragment = doc.createDocumentFragment();
       const $tr = doc.createElement('tr');
-      const $tdImage = doc.createElement('td');
       const $tdBrand = doc.createElement('td');
+      const $tdImage = doc.createElement('td');
       const $tdYear = doc.createElement('td');
       const $tdPlate = doc.createElement('td');
       const $tdColor = doc.createElement('td');
 
-
       const value = $inputs.map(item => item.value)
-  
-      $tdImage.appendChild(createImage(value[0]));
-      $tdBrand.innerHTML = value[1];
-      $tdYear.innerHTML  = value[2];
+
+      $tdBrand.innerHTML = value[0];
+      $tdImage.appendChild(createImage(value[1]));
+      $tdYear.innerHTML = value[2];
       $tdPlate.innerHTML = value[3];
       $tdColor.innerHTML = value[4];
 
@@ -85,14 +80,70 @@
       $tr.appendChild(createButtonRemove());
 
       return $fragment.appendChild($tr);
+    }
+
+    function generateURL() {
+      return $inputs.reduce(function(acc, item, index) {
+        if (index === 1) {
+          acc = 'brandname' + '=' + acc.value + '&' + acc.name + '=' + acc.value + '&' + item.name + '=' + item.value;
+        } else {
+          acc = acc + '&' + item.name + '=' + item.value;
+        }
+
+        return acc;
+      }, 0);
 
     }
 
-    function handlePost(e) {
+    function requestPOST() {
+      ajxPost.open('POST', 'http://localhost:3000/car');
+      ajxPost.setRequestHeader(
+        'Content-Type',
+        'application/x-www-form-urlencoded'
+      );
+      const url = generateURL();
+      ajxPost.send(url);
+
+      ajxPost.addEventListener('readystatechange', handlePost, false);
+    }
+
+    function handlePost() {
+      if (isRequestOK(ajxPost))
+        parserData(ajxPost);
+    }
+
+    function requestGET() {
+      const $ulrcar = $getCar.value;
+      ajxGET.open('GET', `http://localhost:3000/car/${$ulrcar}`);
+      ajxGET.send();
+
+      ajxGET.addEventListener('readystatechange', handleGET, false);
+    }
+
+    function handleGET() {
+      if (isRequestOK(ajxGET))
+        var data = parserData(ajxGET);
+       return data;
+    }
+
+    function isRequestOK(name) {
+      return name.readyState === 4 && name.status === 200;
+    }
+
+    function parserData(name) {
+      return JSON.parse(name.responseText);
+    }
+
+    function handleForm(e) {
       $contentTable.appendChild(createTableElements());
+      requestPOST();
       e.preventDefault();
     }
 
+    function handleFormGET(e) {
+      requestGET();
+      e.preventDefault();
+    }
 
     return {
       requestGetCompany: requestGetCompany,
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,30 +3,46 @@
 var express = require('express');
 var router = express.Router();
 var data = [{
-  image: 'http://blog.caranddriver.com/wp-content/uploads/2015/11/BMW-2-series.jpg',
-  brandModel: 'BMW-2-series',
-  year: '2015',
-  plate: 'bmw-4625',
-  color: 'Red'
-},
-{
-  image: 'https://pictures.topspeed.com/IMG/crop/201511/1984-ferrari-testarossa-44_600x0w.jpg',
-  brandModel: 'Ferrari Testarossa',
-  year: '1993',
-  plate: 'kvs-4822',
-  color: 'Red'
-}
+	  brandname:'BMW-2-series',
+	  brand: 'BMW-2-series',
+    image: 'http://blog.caranddriver.com/wp-content/uploads/2015/11/BMW-2-series.jpg',
+    year: '2015',
+    plate: 'bmw-4625',
+    color: 'Red'
+  },
+  {
+  	brandname:'Ferrari-Testarossa',
+  	brand: 'Ferrari-Testarossa',
+    image: 'https://pictures.topspeed.com/IMG/crop/201511/1984-ferrari-testarossa-44_600x0w.jpg',
+    year: '1993',
+    plate: 'kvs-4822',
+    color: 'Red'
+  }
 ];
 
 router.get('/', function(req, res) {
   res.json(data);
 });
 
+router.get('/:brandname', function(req, res) {
+  var brandname = req.params.brandname;
+  var hasCar = data.some(function(car) {
+    return car.brandname === brandname;
+  });
+
+  if (hasCar) {
+    return res.json(data.filter(function(car) {
+      return car.brandname === brandname;
+    }));
+  }
+});
+
 router.post('/', function(req, res) {
   data.push({
+  	brandname: req.body.brandname,
     image: req.body.image,
-    brandModel: req.body.brandModel,
-    year: req.body.year,
+    brand: req.body.brand,
+    year:  req.body.year,
     plate: req.body.plate,
     color: req.body.color
   });
